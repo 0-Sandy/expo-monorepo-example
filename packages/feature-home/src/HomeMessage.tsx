4917ebf23016196411d5e54f1e9aae71b1929506
Martin <martinrod0012@gmail.com>
2025-02-25 22:45:12 -0300

    feat: Add a localization system and update package scope
    - Added a localization system.
    - Changed package scope from @acme to @repo.

--- packages/feature-home/src/HomeMessage.tsx
+++ packages/feature-home/src/HomeMessage.tsx
@@ -1,10 +1,11 @@
-import { Paragraph, Strong } from '@acme/ui';
+import Lang from '@repo/lang';
+import { Paragraph } from '@repo/ui';
 import { ComponentProps } from 'react';
 
 type HomeMessageProps = ComponentProps<typeof Paragraph>;
 
 export const HomeMessage = (props: HomeMessageProps) => (
   <Paragraph {...props}>
-    Hello from an <Strong>Expo monorepo</Strong>!
+    <Lang.getComponent textKey="welcome" />
   </Paragraph>
 );
